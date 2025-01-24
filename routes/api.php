0ad209f9442413ff58a46eec82273b1099ef310a
Ruth James <jamesruth449@gmail.com>
2025-01-24 11:40:26 +0100

    user controller index action and routes implemented

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\AdminMiddleware;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,5 +15,15 @@ Route::prefix('auth')->group(function () {
         Route::post('register', 'register')->name('auth.register');
         Route::post('login', 'login')->name('auth.login');
         Route::post('logout', 'logout')->middleware('auth:api')->name('auth.logout');
+    });
+});
+
+Route::middleware('auth:api')->group(function () {
+    Route::controller(UserController::class)->group(function () {
+        
+        // Admin Routes
+        Route::middleware(AdminMiddleware::class)->group(function () {
+            Route::get('users', 'index')->name('users.index');
+        });
     });
 });
