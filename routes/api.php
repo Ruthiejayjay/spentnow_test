eec54cc74f36baba2d12fd3a3d86637fa465247d
Ruth James <jamesruth449@gmail.com>
2025-01-24 11:06:27 +0100

    logout logic implemented

--- routes/api.php
+++ routes/api.php
@@ -12,5 +12,6 @@ Route::prefix('auth')->group(function () {
     Route::controller(AuthController::class)->group(function () {
         Route::post('register', 'register')->name('auth.register');
         Route::post('login', 'login')->name('auth.login');
+        Route::post('logout', 'logout')->middleware('auth:api')->name('auth.logout');
     });
 });
